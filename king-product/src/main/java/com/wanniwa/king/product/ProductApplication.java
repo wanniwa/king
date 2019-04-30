ef97d5ca0cf5fe97b8f11dfb98b516a2d5011c43
wanniwa <734933034.qq.com>
2019-04-30 11:16:28 +0800

    eureka

--- king-product/src/main/java/com/wanniwa/king/product/ProductApplication.java
+++ king-product/src/main/java/com/wanniwa/king/product/ProductApplication.java
@@ -1,11 +1,12 @@
 package com.wanniwa.king.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@MapperScan("com.wanniwa.king.product.mapper")
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
