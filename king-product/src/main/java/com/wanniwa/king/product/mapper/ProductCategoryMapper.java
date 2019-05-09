dba7af25fa6be7341d3c7175bc228a4fb535956a
wanniwa <734933034@qq.com>
2019-05-10 00:08:54 +0800

    config

--- king-product/src/main/java/com/wanniwa/king/product/mapper/ProductCategoryMapper.java
+++ king-product/src/main/java/com/wanniwa/king/product/mapper/ProductCategoryMapper.java
@@ -1,7 +1,4 @@
 package com.wanniwa.king.product.mapper;
-import org.apache.ibatis.annotations.Param;
-import java.util.List;
-import java.util.Collection;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wanniwa.king.product.entity.ProductCategory;
