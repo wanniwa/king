3968868b7c4ed6213ffb02c40ed7a42edbe03f82
wanniwa <734933034@qq.com>
2019-07-04 23:33:27 +0800

    权限服务

---
+++ king-auth/king-auth-web/src/main/java/com/wanniwa/king/auth/constant/RedisConstant.java
@@ -0,0 +1,5 @@
+package com.wanniwa.king.auth.constant;
+
+public interface RedisConstant {
+    String TOKEN_TEMPLATE = "token_%s";
+}
--- king-auth/king-auth-web/src/main/java/com/wanniwa/king/auth/controller/LoginController.java
+++ king-auth/king-auth-web/src/main/java/com/wanniwa/king/auth/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.wanniwa.king.auth.controller;
 
 import com.wanniwa.king.auth.constant.CookieConstant;
+import com.wanniwa.king.auth.constant.RedisConstant;
 import com.wanniwa.king.auth.entity.UserInfo;
 import com.wanniwa.king.auth.service.UserInfoService;
 import com.wanniwa.king.common.enums.ResultEnum;
@@ -8,11 +9,14 @@ import com.wanniwa.king.common.enums.RoleEnum;
 import com.wanniwa.king.common.utils.CookieUtil;
 import com.wanniwa.king.common.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wanniwa
@@ -22,32 +26,44 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController {
     @Autowired
     private UserInfoService userInfoService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/buyer")
-    public Result buyer (String openId, HttpServletResponse response){
+    public Result buyer(String openId, HttpServletResponse response) {
         //openId和数据库比对
         UserInfo userInfo = userInfoService.findByOpenId(openId);
-        if (userInfo==null) {
+        if (userInfo == null) {
             return Result.error(ResultEnum.LOGIN_ERROR);
         }
         //判断角色
-        if (RoleEnum.BUYER.getCode() == userInfo.getRole()) {
+        if (RoleEnum.BUYER.getCode() != userInfo.getRole()) {
             return Result.error(ResultEnum.ROLE_ERROR);
         }
         //cookie里设置openId
-        CookieUtil.set(response, CookieConstant.OPENID,openId,CookieConstant.EXPIRE);
+        CookieUtil.set(response, CookieConstant.OPENID, openId, CookieConstant.EXPIRE);
         return Result.ok();
     }
+
     @GetMapping("/seller")
-    public Result seller (String openId, HttpServletResponse response){
+    public Result seller(String openId, HttpServletResponse response) {
         //openId和数据库比对
         UserInfo userInfo = userInfoService.findByOpenId(openId);
-        if (userInfo==null) {
+        if (userInfo == null) {
             return Result.error(ResultEnum.LOGIN_ERROR);
         }
         //判断角色
-        if (RoleEnum.BUYER.getCode() == userInfo.getRole()) {
+        if (RoleEnum.SELLER.getCode() != userInfo.getRole()) {
             return Result.error(ResultEnum.ROLE_ERROR);
         }
+        //redis设置key=UUID
+        String token = UUID.randomUUID().toString();
+        int expire = CookieConstant.EXPIRE;
+        stringRedisTemplate.opsForValue().set(String.format(RedisConstant.TOKEN_TEMPLATE, token),
+                openId, expire, TimeUnit.SECONDS
+        );
+        //cookie里设置openId
+        CookieUtil.set(response, CookieConstant.TOKEN, token, CookieConstant.EXPIRE);
         return Result.ok();
     }
 }
